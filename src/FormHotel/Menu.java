b96a2bb54934f67d41a9da677c6af65cf2d930ce
Zaky Dafalas Eka Putra <zakydaffeka26@gmail.com>
2023-07-14 20:12:42 +0700

    DONE ALL

--- src/FormHotel/Menu.java
+++ src/FormHotel/Menu.java
@@ -34,29 +34,31 @@ public class Menu extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(51, 204, 255));
-        jPanel1.setBorder(javax.swing.BorderFactory.createCompoundBorder());
+        jPanel1.setBackground(new java.awt.Color(0, 204, 204));
+        jPanel1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
 
-        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        jLabel1.setFont(new java.awt.Font("Segoe Print", 1, 24)); // NOI18N
         jLabel1.setText("MENU");
+        jLabel1.setBorder(new javax.swing.border.MatteBorder(null));
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(237, 237, 237)
-                .addComponent(jLabel1)
+                .addGap(220, 220, 220)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
+                .addGap(19, 19, 19)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(19, Short.MAX_VALUE))
+                .addContainerGap(24, Short.MAX_VALUE))
         );
 
+        manage_rooms.setBackground(new java.awt.Color(0, 204, 204));
         manage_rooms.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         manage_rooms.setText("Manage Rooms");
         manage_rooms.addActionListener(new java.awt.event.ActionListener() {
@@ -65,6 +67,7 @@ public class Menu extends javax.swing.JFrame {
             }
         });
 
+        manage_users.setBackground(new java.awt.Color(0, 204, 204));
         manage_users.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         manage_users.setText("Manage Users");
         manage_users.addActionListener(new java.awt.event.ActionListener() {
@@ -73,6 +76,7 @@ public class Menu extends javax.swing.JFrame {
             }
         });
 
+        transaction.setBackground(new java.awt.Color(0, 204, 204));
         transaction.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         transaction.setText("Transaction");
         transaction.addActionListener(new java.awt.event.ActionListener() {
@@ -98,16 +102,17 @@ public class Menu extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(63, 63, 63)
+                .addGap(45, 45, 45)
                 .addComponent(manage_rooms, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(39, 39, 39)
                 .addComponent(manage_users, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(36, 36, 36)
                 .addComponent(transaction, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 112, Short.MAX_VALUE))
+                .addGap(0, 106, Short.MAX_VALUE))
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void manage_roomsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_manage_roomsActionPerformed
